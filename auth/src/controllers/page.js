013ec6d00e1a5011372b47345fb83f574d600b92
ChaejinE <joung6517@gmail.com>
2024-06-03 21:39:55 +0900

    delete

--- auth/src/controllers/page.js
+++ auth/src/controllers/page.js
@@ -6,10 +6,6 @@ exports.renderMain = async (req, res, next) => {
     res.sendFile(path.join(__dirname, "..", `${public_dir_name}/main.html`));
 }
 
-exports.renderLogin = async (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", `${public_dir_name}/login.html`));
-}
-
 exports.renderJoin = async (req, res, next) => {
     res.sendFile(path.join(__dirname, "..", `${public_dir_name}/join.html`));
 }
@@ -20,12 +16,4 @@ exports.renderLoginSuccess = async (req, res, next) => {
 
 exports.renderLoginAlready = async (req, res, next) => {
     res.sendFile(path.join(__dirname, "..", `${public_dir_name}/login-already.html`));
-}
-
-exports.renderLoginRequired = async (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", `${public_dir_name}/login-required.html`));
-}
-
-exports.renderLoginNoRequired = async (req, res, next) => {
-    res.sendFile(path.join(__dirname, "..", `${public_dir_name}/login-no-required.html`));
 }
